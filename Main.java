13c7347ddd7f61a311d1691f2c1fb5537d334f33
Pranav <pranavpatil7743@gmail.com>
2024-02-15 19:26:26 +0530

    Minor change

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         Generator generator = new Generator(sc);
         generator.passwordGen();
